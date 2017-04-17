673f329548d78652c18cc3159f5b8497a0c780a7
Thirat <whalefrog@hotmail.com>
2017-04-18 00:28:53 +0700

    add annotation style

--- SpringBoot/src/main/java/th/com/entity/Student.java
+++ SpringBoot/src/main/java/th/com/entity/Student.java
@@ -1,16 +1,20 @@
 package th.com.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="student" )
+@Table(name="student")
 public class Student {
 	
 	@Id
+	@Column(name = "id",nullable = false, unique = true)
 	Integer id;
+	@Column(name = "sname")
 	String name;
+	@Column(name = "age")
 	Integer age;
 	
 	public Student() {
@@ -27,10 +31,11 @@ public class Student {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	public Integer getAge() {
 		return age;
 	}
@@ -38,7 +43,7 @@ public class Student {
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
